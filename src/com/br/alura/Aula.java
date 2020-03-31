828be23c1ff6d48cb4d6f98fdb03de99ec1261d6
brunamaria <Brq@1234>
2020-03-31 18:15:26 -0300

    Iniciando o curso de coleções.

---
+++ src/com/br/alura/Aula.java
@@ -0,0 +1,40 @@
+package com.br.alura;
+
+public class Aula implements Comparable<Aula> {
+
+    private String titulo;
+    private int tempo;
+
+    public Aula(String titulo, int tempo) {
+        this.titulo = titulo;
+        this.tempo = tempo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(int tempo) {
+        this.tempo = tempo;
+    }
+
+    @Override
+    public String toString() {
+        return "[ Aula " + titulo +
+                ", tempo " + tempo + " minutos" +
+                ']';
+    }
+
+    @Override
+    public int compareTo(Aula outraAula) {
+        return this.titulo.compareTo(outraAula.titulo);
+    }
+}
---
+++ src/com/br/alura/Main.java
@@ -0,0 +1,53 @@
+package com.br.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.Callable;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String aula1 = "Cohecendo mais de Listas";
+        String aula2 = "Modelando a classe Aula";
+        String aula3 = "Trabalhando com Cursos e Sets";
+
+//        String curso1 = "Java 8: Tire proveito dos novos recursos da linguagem";
+//        String curso2 = "Apache Camel";
+//        String curso3 = "Certificacao Java SE 8 Programmer I";
+
+        ArrayList<String> aulas = new ArrayList<>();
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+
+        System.out.println(aulas);
+
+        aulas.remove(0);
+
+        System.out.println(aulas);
+
+        for (String aula : aulas) {
+            System.out.println("Aula: " + aula);
+        }
+
+        String primeiraAula = aulas.get(0);
+        System.out.println("A primeira aula é " + primeiraAula);
+
+        for (int i=0; i< aulas.size(); i++) {
+            System.out.println("Aula: " + aulas.get(i));
+        }
+
+        aulas.forEach(aula -> {
+            System.out.println("Percorrendo:");
+            System.out.println("Aula " + aula);
+        });
+
+        aulas.add("Aumentando o nosso conhecimento");
+        System.out.println(aulas);
+
+        Collections.sort(aulas);
+        System.out.println("Depois de ordenado:");
+        System.out.println(aulas);
+    }
+}
